50ecb88a82d5f29ef5b05147fe33d3863e3a7095
Motoaki Tanaka <katachiwotodomete@gmail.com>
2026-01-16 10:19:23 +0900

    feat: Jest設定を更新し、テストマッチとカバレッジ収集の設定を追加

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,11 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+    testMatch: ['<rootDir>/force-app/**/__tests__/**/*.test.js'],
+    collectCoverageFrom: [
+        '**/force-app/**/*.js',
+        '!**/__tests__/**',
+        '!**/node_modules/**'
+    ]
 };
